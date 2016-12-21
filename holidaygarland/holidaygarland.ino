d7c512886cc7954e3969fe17964f3d2f0afe0a21
PunqRot <cdlawson@coldshadow.com>
2016-12-21 17:19:05 -0600

    v0.2b - Comments added

--- holidaygarland/holidaygarland.ino
+++ holidaygarland/holidaygarland.ino
@@ -1,3 +1,10 @@
+/*
+  Holiday Garland
+  As seen on: http://verbosemode.tv/holiday-garland
+  Created by: Chad Lawson
+  Available at: http://github.com/VerboseModeTV/holidaygarland
+*/
+
 #include <Adafruit_NeoPixel.h>
 
 #define PIN 11
@@ -5,10 +12,11 @@
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM, PIN, NEO_RGB + NEO_KHZ400);
 #define BRIGHT 64 // The default of 255 was SUPER bright, so this cuts it down to 25% of max
-#define PAUSE 250
+#define PAUSE 250 // Sets the amount of milliseconds to pause between changes.
 
+// Colors             Red                   Green                 Gold                    Blue
 uint32_t colors[] = { strip.Color(255,0,0), strip.Color(0,255,0), strip.Color(255,215,0), strip.Color(0, 0, 255) };
-int string[NUM];
+int string[NUM]; // This holds the index into the above array for each pixel.
 
 void setup() {
   strip.begin();
@@ -30,8 +38,8 @@ void setup() {
 }
 
 void loop() {
-    int x=random(1,strip.numPixels()-1);
-    int b=chooseOne(x);
+    int x=random(1,strip.numPixels()-1); // Didn't want to mess with extra code for 0 or max.
+    int b=chooseOne(x); // Function below
     Serial.print("Setting ");
     Serial.print(x);
     Serial.print(" to ");
@@ -44,7 +52,7 @@ void loop() {
 }
 
 int chooseOne(int x) {
-  int avail[] = {0};
+  int avail[] = {0}; // Initialize array of options for next color
   int pos = 0;
   int current = string[x];
   int prev = string[x-1];
@@ -57,6 +65,7 @@ int chooseOne(int x) {
   Serial.println();
 
   Serial.print("Adding ");
+  // Loop finds all options that are not the current or adjacent colors
   for (int x=0 ; x<4 ; x++) {
     if( (x != current) && (x != prev) && (x != next) ) {
       Serial.print(x);
